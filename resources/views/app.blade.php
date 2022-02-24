2ea50ca174f1abb32239f1a2a1de5b473ac70b3d
brijeshcode <brieshchaturvedi.it@gmail.com>
2022-02-24 17:01:57 +0530

    deploay testing

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,9 +4,9 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        {{-- @env ('production')
+        @env ('production')
             <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
-        @endenv --}}
+        @endenv
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
         <!-- Fonts -->
